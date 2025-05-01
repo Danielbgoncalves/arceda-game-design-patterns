7cf7d6d83b0d254e31e144bbf642d26ec64e24f0
Daniel Borges <44danbor@gmail.com>
2025-04-30 23:32:43 -0300

    comentários

--- spaceinvaders/SpaceInvadersGame.java
+++ spaceinvaders/SpaceInvadersGame.java
@@ -26,3 +26,19 @@ public class SpaceInvadersGame extends MainFrame {
 	}
 
 }
+
+/** A SER REALIZADO
+ *  1- mudar alien para monstros
+ *  2- fazer mosntros andares em vários directions
+ *  3- colocar woody
+ *  4- congelar monstros, não só sumir
+ *  5- mudar a img de fundo (p/ uma com nuvens brancas!)
+ *
+ */
+
+/** TA FAZIDO !
+ * 1- movimentação do bixinhos de forma bonita e seguindo padrões !
+ * 2- arma atira pra to do canto
+ * 3- entendendo como funciona ISSO <- talvez maior feito do dia
+ * */
+
